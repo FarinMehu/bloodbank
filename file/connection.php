ff3610181faef3a564f513b4beead32679cdd286
Mehjabin Khan Farin <80196064+FarinMehu@users.noreply.github.com>
2021-07-03 02:04:01 -0700

    added

---
+++ file/connection.php
@@ -0,0 +1,11 @@
+<?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "bloodbank";
+$conn = mysqli_connect("localhost", "root", "", "bloodbank") or die("
+Could not connect Mysql." .mysqli_connect_error());
+//if(!$conn){
+ //die('Could not Connect MySql:' .mysql_error());
+//}
+?>
